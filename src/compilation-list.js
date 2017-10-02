34e4a38c192cea93e494366b3d0df341ea5645f4
Zhuo Lu <sethlu@mintkit.net>
2017-10-01 23:19:25 -0700

    Update compilation list time format

--- src/compilation-list.js
+++ src/compilation-list.js
@@ -1,11 +1,26 @@
 
+"use strict";
+
 const path = require("path");
 const fs = require("fs-extra");
 const replaceExt = require("replace-ext");
 
+function getTimestamp(str) {
+  let parts = str.split("-");
+  let len = parts.length;
+  if (len >= 3 && len <= 5) {
+    let date = [parts[0], parts[1], parts[2]].join("/");
+    if (len >= 4) {
+      date += ` ${parts.slice(3).join(":")} pm`;
+    }
+    return date;
+  }
+  return str;
+}
+
 function html() {
   return fs.readdirSync(path.join(__dirname, "compilations"))
-    .map(file => `- {.link ${path.join("compilations", file)} | ${replaceExt(file, "")}}`)
+    .map(file => `- {.link ${path.join("compilations", file)} | ${getTimestamp(replaceExt(file, ""))}}`)
     .join("\n");
 }
 
