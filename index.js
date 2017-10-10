a8f0253653c8e84ccf94f9a1bf1a47b599c464c6
Zhuo Lu <sethlu@mintkit.net>
2017-10-10 02:42:08 -0700

    Fix timestamp past midnight

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ converter.setFlavor("github");
 // Timestamp
 function getTimestamp() {
   let date = new Date();
-  return `${[date.getMonth() + 1, date.getDate(), date.getFullYear() - 2000].join("/")} ${[date.getHours() - 12, date.getMinutes().toString().padStart(2, "0")].join(":")} pm`;
+  return `${[date.getMonth() + 1, date.getDate(), date.getFullYear() - 2000].join("/")} ${[date.getHours() % 12, date.getMinutes().toString().padStart(2, "0")].join(":")} ${date.getHours() < 12 ? "am" : "pm"}`;
 }
 
 // Function to build a Markdown file
