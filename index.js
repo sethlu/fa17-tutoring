7cd47b37f490644d5c4c675f4e397ae7ab160b7d
Zhuo Lu <sethlu@mintkit.net>
2017-10-11 23:33:46 -0700

    Adjust timestamp format

--- index.js
+++ index.js
@@ -8,27 +8,6 @@ const showdownHighlight = require("showdown-highlight");
 const replaceExt = require("replace-ext");
 const util = require("./util");
 
-// Polyfill
-
-// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
-if (!String.prototype.padStart) {
-  String.prototype.padStart = function padStart(targetLength,padString) {
-    targetLength = targetLength>>0; //floor if number or convert non-number to 0;
-    padString = String(padString || ' ');
-    if (this.length > targetLength) {
-      return String(this);
-    }
-    else {
-      targetLength = targetLength-this.length;
-      if (targetLength > padString.length) {
-        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
-      }
-      return padString.slice(0,targetLength) + String(this);
-    }
-  };
-}
-
 // Regex's
 let regexLink = /{\.link\s+([^|]*)(?:\s+\|\s+([^}]*))?\s*}/g;
 let regexInclude = /{\.include\s+([^}]*)\s*}/g;
--- util.js
+++ util.js
@@ -1,11 +1,38 @@
 
 "use strict";
 
+// Polyfill
+
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength,padString) {
+    targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+    padString = String(padString || ' ');
+    if (this.length > targetLength) {
+      return String(this);
+    }
+    else {
+      targetLength = targetLength-this.length;
+      if (targetLength > padString.length) {
+        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+      }
+      return padString.slice(0,targetLength) + String(this);
+    }
+  };
+}
+
+// Format timestamp
+
 module.exports.getTimestamp = function (date) {
   if (!date) date = new Date();
   let text = [date.getMonth() + 1, date.getDate(), date.getFullYear() - 2000].join("/");
   if (date.getHours() > 0 || date.getMinutes() > 0) {
-    text += " " + [date.getHours() % 12, date.getMinutes().toString().padStart(2, "0")].join(":") + " " + (date.getHours() < 12 ? "am" : "pm");
+    text += " " + (date.getHours() % 12);
+    if (date.getMinutes() > 0) {
+      text += ":" + date.getMinutes().toString().padStart(2, "0");
+    }
+    text += " " + (date.getHours() < 12 ? "am" : "pm");
   }
   return text;
 }
